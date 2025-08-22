ed838f43dd32f86ffbd2a48a5a3bbef334a23db3
Rimsha Liaqat <rimsha.liaqat@auxilium.digital>
2025-08-22 15:54:47 +0500

    Required Columns

--- TaskPro/Models/Task.cs
+++ TaskPro/Models/Task.cs
@@ -1,18 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace TaskPro.Models;
 
 public partial class Task
 {
     public int Id { get; set; }
-
+    [Required]
     public string Title { get; set; } = null!;
-
+    [Required]
     public string Description { get; set; } = null!;
-
+    [Required
     public int Priority { get; set; }
-
+    [Required]
     public DateTime Deadline { get; set; }
 
     public string CreatedBy { get; set; } = null!;
